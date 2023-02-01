e44374f4c06f0e622c6a29b17bedb18f775f18b8
Lucas Farias <lucasf1@gmail.com>
2023-02-01 18:55:42 -0300

    Projeto com autenticação basic

---
+++ src/test/java/io/github/lucasf1/sbsecurity/SbSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucasf1.sbsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
